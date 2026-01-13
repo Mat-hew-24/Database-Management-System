2b403814db25969e572a3d924bd92d44d6febf00
mat-hew-24 <amonline2005@gmail.com>
2026-01-13 19:13:59 +0530

    stage2 working

--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -5,6 +5,21 @@
 #include "cstring"
 #include "iostream"
 
+void stage1()
+{
+  unsigned char buffer[BLOCK_SIZE];
+  char message[] = "hello";
+  memcpy(buffer + 20, message, 6); // buffer[20] = 'h' , buffer[21] = 'e' , => 1st param, pointer is 2nd param is msg and 3rd param is its size => 'hello\0' is 6 bytes
+  Disk::writeBlock(buffer, 7000);  // write the buffer into that block
+
+  // PRINTING THE MESSAGE AFTER RETRIEVING FROM THE BLOCK WE JUST WROTE INTO
+  unsigned char buffer2[BLOCK_SIZE];
+  Disk::readBlock(buffer2, 7000);
+  char message2[6];
+  memcpy(message2, buffer2 + 20, 6);
+  std::cout << message2 << "\n";
+}
+
 // -------- MEANS IT WAS ALREADY THERE (THE COMMENT)
 void stage1qn1()
 {
@@ -26,27 +41,57 @@ void stage1qn1()
   }
 }
 
+void stage2()
+{
+
+  // create objects for the relation catalog and attribute catalog
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+
+  HeadInfo relCatHeader;
+  HeadInfo attrCatHeader;
+
+  // load the headers of both the blocks into relCatHeader and attrCatHeader.
+  // (we will implement these functions later)
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
+
+  int no_of_relations = relCatHeader.numEntries;
+
+  for (int i = 0; i < no_of_relations; i++)
+  {
+    Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
+    relCatBuffer.getRecord(relCatRecord, i);
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+    int no_of_attr = attrCatHeader.numEntries;
+
+    for (int j = 0; j < no_of_attr; j++)
+    {
+
+      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+      attrCatBuffer.getRecord(attrCatRecord, j);
+
+      if (strcmp(relCatRecord[RELCAT_REL_NAME_INDEX].sVal, attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal) == 0)
+      {
+        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+        printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+      }
+    }
+    printf("\n");
+  }
+}
+
 int main(int argc, char *argv[])
 {
   /*--------- Initialize the Run Copy of Disk ---------- */
   Disk disk_run;
 
   /*---------- StaticBuffer buffer;  --------*/
-  unsigned char buffer[BLOCK_SIZE];
-  char message[] = "hello";
-  memcpy(buffer + 20, message, 6); // buffer[20] = 'h' , buffer[21] = 'e' , => 1st param, pointer is 2nd param is msg and 3rd param is its size => 'hello\0' is 6 bytes
-  Disk::writeBlock(buffer, 7000);  // write the buffer into that block
-
-  // PRINTING THE MESSAGE AFTER RETRIEVING FROM THE BLOCK WE JUST WROTE INTO
-  unsigned char buffer2[BLOCK_SIZE];
-  Disk::readBlock(buffer2, 7000);
-  char message2[6];
-  memcpy(message2, buffer2 + 20, 6);
-  std::cout << message2;
-
-  // BLOCK ALLOCATION MAP QN verification
-  // stage1qn1();
-
+  // stage1();
+  //  BLOCK ALLOCATION MAP QN verification
+  //  stage1qn1();
+  stage2();
   return 0;
   /*------------- OpenRelTable cache ------------ */;
 
